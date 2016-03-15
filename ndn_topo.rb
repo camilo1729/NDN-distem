d37e491a447f7ce10eec55eb1c68c33d698d8300
Cristian Ruiz <cristian.ruiz@inria.fr>
2016-03-15 16:34:20 +0100

    Fixed ndn_topo script

--- ndn_topo.rb
+++ ndn_topo.rb
@@ -4,6 +4,7 @@ require 'distem'
 require 'yaml'
 require 'cute'
 require 'ipaddr'
+require 'net/scp'
 load 'nlsrcGen.rb'
 
 FSIMG="/home/cruizsanabria/distemIMGjessieNLSR3bis.tar.gz"
@@ -112,32 +113,6 @@ topo.keys.each do |vnode|
 end
 
 
-###### Deployed infra #################
-Net::SSH::Multi.start do |session|
-
-   session.group :vnodes do
-     topo.keys.each { |vnode| session.use("root@#{vnode}",{:paranoid => false})}
-   end
-
-   # test connection
-   #
-
-   [
-     "mkdir -p /var/log/nlsr",
-     "mkdir -p /var/lib/nlsr",
-     "nohup nfd-start < /dev/null >./log 2>./log-err",
-     "sleep 2"
-  ].each { |x| session.with(:vnodes).exec! x }
-
-end
-
-puts "ssh"
-
-topo.keys.each do |x|
-  puts "On #{x}"
-  system('ssh',"root@#{x}","echo -e '#{hosts[x]}' | dd of=/etc/hosts")
-  system "ssh root@#{x} screen -d -m /root/nlsr-start.sh"
-end
 
 # Cute::TakTuk.start(topo.keys,:connector => 'ssh -o StrictHostKeyChecking=no',:user => 'root') do |tak|
 
