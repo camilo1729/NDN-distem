46f1b62451eca35890f95d33a0b3f0b9066c3de6
Cristian Ruiz <cristian.ruiz@inria.fr>
2016-08-02 12:48:15 +0200

    Improve ping all test

--- ping_all_test.rb
+++ ping_all_test.rb
@@ -1,6 +1,6 @@
 require 'yaml'
 require 'cute'
-
+require 'distem'
 # put create data packages on one node
 
 
@@ -15,38 +15,65 @@ end
 #nodes.delete("uiuc")
 
 # Initializing ping server
+Distem.client do |cl|
+
+  nodes.each do |node_to_kill|
+
+    `ruby ndn_stop.rb ndn_testbed_topo.yaml`
+    sleep 2
+    `ruby ndn_start.rb  ndn_testbed_topo.yaml`
+    sleep 10
+    `ruby initialize_ping_server.rb`
+
+  # checking that all the servers have been initiazlied
+
+    num_server = "ps aux | grep ndnping | wc -l ".chop
+
+    if num_server != "45"
+      puts "problemm initializing exiting"
+      exit
+    end
+
+    results = {}
+    Net::SSH::Multi.start do |session|
+
+      session.group :vnodes do
+        nodes.each{ |vnode| session.use("#{vnode}-adm",{:user => "root",:paranoid => false})}
+      end
+
+      nodes.each do |node|
+        session.with(:vnodes).exec! "nohup ndnping -c 200 /ndn/nodeAnnounce#{node} > #{node}.txt &"
+      end
+
+    end
 
 
-results = {}
-Net::SSH::Multi.start do |session|
+    puts "Killing node: #{node_to_kill}"
+    sleep 20
+    cl.vnode_stop(node_to_kill)
+    sleep 200
 
-  session.group :vnodes do
-    nodes.each{ |vnode| session.use("#{vnode}-adm",{:user => "root",:paranoid => false})}
+
+    temp_nodes = nodes.clone
+    temp_nodes.delete(node_to_kill)
+
+    `mkdir -p results_#{node_to_kill}`
+
+    temp_nodes.each do |vnode|
+
+      dir_node = "results_#{node_to_kill}/#{vnode}"
+      `mkdir -p #{dir_node}`
+      `scp #{vnode}-adm:~/*.txt #{dir_node}/`
+
+      Net::SSH.start("#{vnode}-adm", "root") do |ssh|
+        puts "Deleting files On #{vnode}"
+        ssh.exec "rm *.txt"
+      end
+
+    end
+
+    puts "Restarting node: #{node_to_kill}"
+    cl.vnode_start(node_to_kill)
+    sleep 10
   end
-
-  nodes.each do |node|
-    session.with(:vnodes).exec! "nohup ndnping -c 100 /ndn/nodeAnnounce#{node} > #{node}.txt &"
-  end
-
-end
-
-
-sleep 200
-
-node_delete = "uiuc"
-nodes.delete(node_delete)
-
-`mkdir -p results_#{node_delete}`
-
-nodes.each do |vnode|
-
-  dir_node = "results_#{node_delete}/#{vnode}"
-  `mkdir -p #{dir_node}`
-  `scp #{vnode}-adm:~/*.txt #{dir_node}/`
-
-  Net::SSH.start("#{vnode}-adm", "root") do |ssh|
-    puts "Deleting files On #{vnode}"
-    ssh.exec "rm *.txt"
-  end
-
 end
