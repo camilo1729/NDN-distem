8a15087507a36440e57480238d983d17518aee9e
Cristian Ruiz <cristian.ruiz@inria.fr>
2016-08-26 18:09:35 +0200

    Added generation of configuration files

--- peer-topology.rb
+++ peer-topology.rb
@@ -2,12 +2,14 @@
 require 'distem'
 
 NDNIMG="/home/cruizsanabria/jessie-ndn-lxc.tar.gz"
-
-
 numpeers = 16
 vnetwork = IPAddress::IPv4.new("10.144.0.0/18")
 
 subnets = vnetwork.subnet(24)
+
+dir_config = "nslr_config"
+Dir.mkdir dir_config
+
 Distem.client { |cl|
 
   vnodes = []
@@ -15,8 +17,10 @@ Distem.client { |cl|
 
     cl.vnetwork_create("vnet#{x}", subnets[x].to_string, :network_type => 'vxlan')
 
+    node1 = "n#{x}"
+    node2 = "n#{x}-1"
     ips = subnets[x].map{ |ip| ip.to_s}
-    cl.vnode_create("n#{x}",
+    cl.vnode_create(node1,
                   {
 #                   'host' => machine,
                    'vfilesystem' =>{'image' => NDNIMG,'cow' => true},
@@ -25,7 +29,7 @@ Distem.client { |cl|
                                 ]
                   })
 
-    cl.vnode_create("n#{x}-1",
+    cl.vnode_create(node2,
                   {
  #                   'host' => machine,
                    'vfilesystem' =>{'image' => NDNIMG,'cow' => true},
@@ -34,8 +38,16 @@ Distem.client { |cl|
                                 ]
                   })
 
-    vnodes+=["n#{x}","n#{x}-1"]
+    vnodes+=[node1,node2]
+
+    puts "Generating configuration files"
+    node = {:site => "s#{x}",:router => "r#{x}", :ip => ips[2]}
+    neighbor = {:site => "s#{x}-1",:router => "r#{x}-1", :ip => ips[3]}
+    File.open("#{dir_config}/nlsr-#{node1}.conf",'w') {|f| f.write confGen(node,neighbor)}
+    File.open("#{dir_config}/nlsr-#{node2}.conf",'w') {|f| f.write confGen(neighbor,node)}
+
   end
+
 
   puts "Starting vnodes..."
   cl.vnodes_start(vnodes)
@@ -48,4 +60,55 @@ Distem.client { |cl|
   else
     puts "vnodes are unreachable"
   end
+
+handler = Proc.new do |server|
+
+  server[:connection_attempts] ||= 0
+  if server[:connection_attempts] < 40
+    server[:connection_attempts] += 1
+    puts "Retrying connection"
+    sleep 5
+    throw :go, :retry
+  else
+    throw :go, :raise
+  end
+end
+
+
+# Testing connection
+Net::SSH::Multi.start(:on_error => handler) do |session|
+   session.group :vnodes do
+     topo.keys.each { |vnode| session.use("root@#{vnode}-adm",{:paranoid => false})}
+   end
+   puts session.with(:vnodes).exec "hostname"
+   # updating library path
+   puts session.with(:vnodes).exec "ldconfig"
+end
+
+
+# we need to transfert file using the admin network
+topo.keys.each do |vnode|
+  # setting admin address
+  node_name = "#{vnode}-adm"
+  Net::SCP.start(node_name,'root') do |scp|
+    conf_file ="root/nlsr-#{vnode}.conf"
+    nlsr_start_file = "root/nlsr-start.sh"
+    puts "uploading #{File.basename(conf_file)} to node: #{node_name}"
+    puts scp.upload! conf_file,File.basename(conf_file)
+    puts scp.upload! nlsr_start_file,File.basename(nlsr_start_file)
+  end
+end
+
+
+# saving host for /etch/hosts
+
+File.open("hosts_helper.yaml",'w') do |f|
+  f.puts(hosts.to_yaml)
+end
+
+File.open("machinefile.txt", 'w') do |f|
+  topo.keys.each { |vnode|  f.puts vnode}
+end
+
+
 }
