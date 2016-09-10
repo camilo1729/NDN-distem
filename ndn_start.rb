0dfa2b80e0e96a2909159c4ba8ee4e8032c6d14f
Cristian Ruiz <cristian.ruiz@inria.fr>
2016-09-10 10:23:43 +0200

    Revert change in ndn_start script

--- ndn_start.rb
+++ ndn_start.rb
@@ -7,20 +7,27 @@ topoFile=ARGV[0]
 topo = (YAML.load(File.open(topoFile,'r') {|f| f.read + "\n---\n"}) || {} )
 hosts = YAML.load(File.open("hosts_helper.yaml",'r') )
 
-nodes = topo.keys
 
-Cute::TakTuk.start(nodes, :user => 'root') do |tak|
+Net::SSH::Multi.start do |session|
 
-  [
+
+   session.group :vnodes do
+     topo.keys.each { |vnode| session.use("root@#{vnode}-adm",{:paranoid => false})}
+   end
+
+   # test connection
+   #
+
+   [
      "mkdir -p /var/log/nlsr",
      "mkdir -p /var/lib/nlsr",
      "nohup nfd-start < /dev/null >./log 2>./log-err",
      "sleep 2"
-  ].each { |x| tak.exec! x }
+  ].each { |x| session.with(:vnodes).exec! x }
+
+
 
 end
-
-
 
 sleep 10
 # updating hosts file and starting nlsr
