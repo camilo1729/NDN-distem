0ec37e2141b0782293260a1b28fe888ad6a80876
Cristian Ruiz <cristian.ruiz@inria.fr>
2016-09-10 15:47:13 +0200

    Fix run cache all

--- run_cache_all.rb
+++ run_cache_all.rb
@@ -5,8 +5,10 @@ runs.times do |x|
   puts "run #{x}"
   dir = "run#{x}"
   `ruby ndn_start.rb fai1.yaml`
-  `cache_test.rb fai1.yaml`
-  `mkdir #{dir}`
-  `mv results* #{dir}`
-  `ruby ndn_stop.rb fai1.yaml`
+  [20,40,80,160,320,640].each do |size|
+    `cache_test.rb #{size}`
+    `mkdir #{dir}`
+    `mv results* #{dir}`
+    `ruby ndn_stop.rb fai1.yaml`
+  end
 end
